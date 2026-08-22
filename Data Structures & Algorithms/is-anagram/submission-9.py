3416ecb9d8fbc18ff2bb4faa858d4690f0509f62
tomtrann <trancamtoan00@gmail.com>
2026-08-22 06:23:40 -0500

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        hashmaps, hashmapt = {}, {}
+
+        for i in range(len(s)):
+            hashmaps[s[i]] = hashmaps.get(s[i], 0) + 1
+            hashmapt[t[i]] = hashmapt.get(t[i], 0) + 1
+        
+        for n in hashmaps:
+            if hashmaps[n] != hashmapt.get(n, 0):
+                return False
+        
+        return True
