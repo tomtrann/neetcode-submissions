0066a1d044f913882d9a7c8aa9d4d14bcdfdfbfc
tomtrann__ <trancamtoan00@gmail.com>
2026-09-03 13:05:52 -0500

    Update: top-k-elements-in-list - submission-1

--- Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -1,22 +1,18 @@
-from collections import Counter
-import heapq
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
       hashmap = {}
 
-      for i in nums:
-        hashmap[i] = hashmap.get(i, 0) + 1
-     
+      for n in nums: 
+        hashmap[n] = hashmap.get(n,0) + 1
+    
       maxheap = []
-      
       for num, freq in hashmap.items():
         maxheap.append((-freq, num))
-     
-      heapq.heapify(maxheap)
-      arr = []
-      for n in range(k):
-        freq, num = heapq.heappop(maxheap)
-        arr.append(num)
     
-      return arr;
+      heapq.heapify(maxheap)
+      res = []
+      for i in range(k):
+        freq, num = heapq.heappop(maxheap)
+        res.append(num)
+
+      return res
