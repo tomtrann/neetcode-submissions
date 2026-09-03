6284ea46a622a73ec91580c19ad486675deba3e9
tomtrann__ <trancamtoan00@gmail.com>
2026-09-03 12:44:55 -0500

    Update: duplicate-integer - submission-1

--- Data Structures & Algorithms/duplicate-integer/submission-1.py
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -1,11 +1,8 @@
-from collections import defaultdict
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        hashmap = defaultdict(int)
-        for i in nums:
-            if hashmap[i] > 0:
-                return True
-            hashmap[i] += 1
-        
-        return False
-        
+       hashset = set() 
+       for n in nums:
+        if n in hashset: 
+            return True
+        hashset.add(n)
+       return False
