29a7f20899b03a733e73f69c96106dcd03adf283
tomtrann__ <trancamtoan00@gmail.com>
2026-09-21 02:12:02 -0500

    Add: add-binary - submission-0

---
+++ Data Structures & Algorithms/add-binary/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a = a[::-1] 
+        b = b[::-1] 
+        carry = 0
+        res = ""
+        for i in range(max(len(a),len(b))):
+            digitA = ord(a[i]) - ord("0") if i < len(a) else 0
+            digitB = ord(b[i]) - ord("0") if i < len(b) else 0
+
+            total = digitA + digitB + carry
+            char = str(total % 2) 
+            res = char + res
+            
+            carry = total // 2
+        
+        if carry:
+            res = "1" + res
+        return res
